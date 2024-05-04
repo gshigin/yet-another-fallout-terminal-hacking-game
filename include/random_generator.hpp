3a2ad979851b30cab30f89c6f4bd4d0ac0a61424
gshigin <gleb.shigin@gmail.com>
2024-05-04 13:28:58 +0300

    better directory managment

---
+++ include/random_generator.hpp
@@ -0,0 +1,68 @@
+#ifndef YAFTH_RANDOM_GENERATOR_H
+#define YAFTH_RANDOM_GENERATOR_H
+
+#include <cstdint>
+
+namespace yafth
+{
+
+    constexpr uint64_t seed(const uint64_t seed_)
+    {
+        uint64_t shifted = seed_;
+
+        // one splitmix64 iteration
+        shifted = (shifted += 0x9e3779b97f4a7c15);
+        shifted = (shifted ^ (shifted >> 30)) * 0xbf58476d1ce4e5b9;
+        shifted = (shifted ^ (shifted >> 27)) * 0x94d049bb133111eb;
+        return shifted ^ (shifted >> 31);
+    }
+
+    // uses time of compilation for seeding rng
+    // good for compile-time randomness
+    constexpr uint64_t seed()
+    {
+        uint64_t shifted = 0;
+        for(const auto c : __TIME__)
+        {
+            shifted <<= 8;
+            shifted |= c;
+        }
+
+        return seed(shifted);
+    }
+
+    struct xoroshiro128
+    {
+        std::uint64_t state[2]{1, 1};
+
+        xoroshiro128() = default;
+
+        constexpr xoroshiro128(uint64_t s0, uint64_t s1) : state{s0, s1} {}
+
+        // or c++20 std::rotl
+        constexpr static inline uint64_t rotl(const uint64_t x, int k) 
+        {
+            return (x << k) | (x >> (64 - k));
+        }
+
+        constexpr std::uint64_t next() noexcept
+        {
+            const auto s0 = state[0];
+            auto s1 = state[1];
+            const auto result = rotl(s0 + s1, 17) + s0;
+
+            s1 ^= s0;
+            state[0] = rotl(s0, 49) ^ s1 ^ (s1 << 21);
+            state[1] = rotl(s1, 28);
+
+            return result >> 4;
+        }
+
+        template <typename T>
+        constexpr T next_t() noexcept { return static_cast<T>(next()); }
+
+        constexpr xoroshiro128 fork() { return xoroshiro128{next(), next()}; }
+    };
+} // namespace yafth
+
+#endif // YAFTH_RANDOM_GENERATOR_H
---
+++ include/yafth_engine.hpp
@@ -0,0 +1,68 @@
+#ifndef YAFTH_ENGINE_H
+#define YAFTH_ENGINE_H
+
+#include <cstdint>
+#include <vector>
+#include <array>
+#include <string>
+#include <algorithm>
+
+namespace yafth
+{
+    // from https://geckwiki.com/index.php?title=Hacking_Word_Count
+    enum class LockLevel
+    {
+        VeryEasy = 0,
+        Easy = 1,
+        Average = 2,
+        Hard = 3,
+        VeryHard = 4
+    };
+
+    class yafth_engine_t
+    {
+    public:
+        yafth_engine_t(const LockLevel lockLevelSetting_, const uint32_t playerScienceSkill_, const uint64_t seed_) 
+            : rng(0, seed(seed_))
+            , lockLevelSetting(lockLevelSetting_)
+            , playerScienceSkill(playerScienceSkill_)
+            , wordLength(set_word_length())
+            , wordCount(set_word_count())
+            , answer(rng.next() % wordCount)
+        {
+            generate_chars_stream();
+            generate_words();
+        }
+
+        uint32_t get_word_count() const {return wordCount;}
+        uint32_t get_word_length() const {return wordLength;}
+        uint32_t get_attempts() const {return attemptsLeft;}
+
+        const std::vector<std::string> & get_words() const {return words;}
+        const std::array<char, 408> & get_chars_stream() const {return chars_stream;}
+
+        const std::string & get_log() const {return log;}
+    private:
+        uint32_t set_word_count();
+        uint32_t set_word_length();
+        void generate_chars_stream();
+        void generate_words();
+
+        xoroshiro128 rng;
+
+        const LockLevel lockLevelSetting;
+        const uint32_t playerScienceSkill;
+        const uint32_t wordLength;
+        const uint32_t wordCount;
+        const uint32_t answer;
+
+        uint32_t attemptsLeft = 4;
+        std::string log = "";
+
+        std::vector<std::string> words;
+        std::array<char, 408> chars_stream;
+        std::vector<decltype(chars_stream.begin())> words_pointers; // pointers to words in chars_stream
+    };
+} // namespace yafth
+
+#endif // YAFTH_ENGINE_H
