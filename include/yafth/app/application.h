cae507a884f3611385b89580bbd62f31f930a9b8
Gleb Shigin <gleb.shigin@gmail.com>
2025-05-21 08:38:38 +0300

    Hide UI implenemtation details (#12)
    * hide ui implementation details and dependency from ftxui
    
    * remove game over check from app

--- include/yafth/app/application.h
+++ include/yafth/app/application.h
@@ -15,12 +15,6 @@ class application
     application(yafth::args args);
     ~application();
 
-    application(application const &) = delete;
-    auto operator=(application const &) -> application & = delete;
-
-    application(application &&) noexcept = delete;
-    auto operator=(application &&) noexcept -> application & = delete;
-
     void run();
 
   private:
